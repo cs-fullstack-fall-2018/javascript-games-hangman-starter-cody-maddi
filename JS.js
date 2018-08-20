a910dcee28dd7e6c39d4ea4732b11f1508b31f5f
Christopher Pace <Christopher_pace@ymail.com>
2018-08-20 14:01:43 -0500

    updated

--- JS.js
+++ JS.js
@@ -1,6 +1,9 @@
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var wordList = ["Awkward", "Bagpipes", "Banjo", "Bungler", "Croquet", "Crypt", "Dwarves", "Fervid", "Fishhook", "Fjord", "Gazebo", "Gypsy", "Haiku", "Haphazard", "Hyphen", "Ivory", "Jazzy", "Jiffy", "Jinx", "Jukebox", "Kayak", "Kiosk", "Klutz", "Memento", "Mystify", "Numbskull", "Ostracize", "Oxygen", "Pajama", "Phlegm", "Pixel", "Polka", "Quad", "Quip", "Rhythmic", "Rogue", "Sphinx", "Squawk", "Swivel", "Toady", "Twelfth", "Unzip", "Waxy", "Wildebeest", "Yacht", "Zealous", "Zigzag", "Zippy", "Zombie"];
 var randomWord = Math.floor(Math.random() * wordList.length);
-console.log(wordList[randomWord]);
-
-var gifs[]
+var chosenWord = wordList[randomWord];
+console.log(chosenWord);
+if () 
+// var strikes = 0;
+// while (strikes<10){
+//     }
